37ada7fcdc0458a970873fe2917a8252416d3b06
SepehrGanji <sepehrganji79@gmail.com>
2022-05-26 20:41:29 +0430

    fix lint

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
-import {UserService} from "./user.service";
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -8,7 +8,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [UserService]
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
