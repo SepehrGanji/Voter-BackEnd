61c000ae53f0a5ff53df9fbb650036f484e85b7f
SepehrGanji <sepehrganji79@gmail.com>
2022-05-26 19:57:05 +0430

    add user module

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port: number = Number.parseInt(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Nest application is running on port ${port}`);
 }
 bootstrap();
