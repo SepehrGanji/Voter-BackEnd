d9606b0b1f126ef40c85e447905babc82fe908b9
SepehrGanji <sepehrganji79@gmail.com>
2022-06-14 17:39:16 +0430

    add temp CORS

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(port);
   console.log(`Nest application is running on port ${port}`);
 }
