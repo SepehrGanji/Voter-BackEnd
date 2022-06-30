d67946550bac5f01536a9ca8c31fd9f69502c7c4
SepehrGanji <sepehrganji79@gmail.com>
2022-06-30 09:31:32 +0430

    increase token expiration time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '10d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
